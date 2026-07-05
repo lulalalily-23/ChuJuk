8e7739965553e8737d40795c155e712c3b635a19
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2026-07-05 18:14:39 +0900

    캐릭터 jump

--- Assets/Character/Scripts/PlayerController.cs
+++ Assets/Character/Scripts/PlayerController.cs
@@ -3,25 +3,56 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 6f;
+
     private Rigidbody rb;
+    private bool isGrounded; // 바닥에 닿아있는지 체크
+    private bool jumpRequested; // 점프 키 눌림여부
 
     void Start()
     {
-        // 시작할 때 내 캡슐에 달려있는 Rigidbody를 찾아서 가져옴
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpRequested = true;
+        }
     }
 
     void FixedUpdate()
     {
-        // 키보드 방향키나 WASD 입력 받기
+        // 이동처리
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
-        // 이동 방향 설정 (x축은 좌우, z축은 앞뒤)
-        // normalized를 안 해주면 대각선으로 갈 때 1.4배 빨라지는 버그 생김
-        Vector3 movement = new Vector3(h, 0f, v).normalized;
-
-        // 실제 이동 처리
+        Vector3 movement = new Vector3(h, 0f, v).normalized; // normalized를 안 해주면 대각선으로 갈 때 1.4배 빨라지는 버그 생김
         rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (jumpRequested)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            jumpRequested = false;
+            isGrounded = false;
+        }
+    }
+
+    // 충돌처리
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Plane"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name.Contains("Plane"))
+        {
+            isGrounded = false;
+        }
     }
 }
