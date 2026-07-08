e5d21db736a7b6ace961102c78db1e62a2eee29e
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2026-07-09 02:28:50 +0900

    dash & run

--- chujuk-game/Assets/Char/Scripts/PlayerController.cs
+++ chujuk-game/Assets/Char/Scripts/PlayerController.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 6f;
+    private float moveSpeed;
+    public float jumpForce;
+    public float Speed;
+    public float dashSpeed;
+
+    public float moveTime;
+    private float dashTime;
 
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool jumpRequested;
+    private bool isDash;
 
     void Start()
     {
+        moveSpeed = Speed;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -34,6 +41,33 @@ public class PlayerController : MonoBehaviour
             jumpRequested = false;
             isGrounded = false;
         }
+
+        /* if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = 6;
+        } else
+        {
+            moveSpeed = Speed;
+        } */
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            isDash = true;
+        }
+
+        if (dashTime <= 0)
+        {
+            // moveSpeed = Speed;
+            if (isDash)
+            {
+                dashTime = moveTime;
+            }
+        } else
+        {
+            dashTime -= Time.deltaTime;
+            moveSpeed = dashSpeed;
+        }
+        isDash = false;
     }
 
     private void OnCollisionStay2D(Collision2D collision)
