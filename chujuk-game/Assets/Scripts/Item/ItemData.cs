978ba2e00a2b66141736bb0fca40db54280e5647
DELL1111AA <applestorytwo@gmail.com>
2026-07-25 15:18:25 +0900

    아이템 데이터 추가
    아이템 데이터 추가

--- chujuk-game/Assets/Scripts/Item/ItemData.cs
+++ chujuk-game/Assets/Scripts/Item/ItemData.cs
@@ -16,22 +16,17 @@ public class ItemData : ScriptableObject
     [TextArea(1, 10)]
     public string description;
 
-
+    [Header("아이템 등급")]
+    public ItemGrade grade;
 
     [Header("아이템 분류")]
     public ItemEffectType effectType;
 
-
-
     [Header("아이템 태그")]
     public List<ItemTag> tags = new List<ItemTag>();
 
-
-
     [Header("능력치 효과")]
     public List<StatModifier> modifiers = new List<StatModifier>();
-
-
 
     [Header("액티브 설정")]
     public float cooldown;
--- chujuk-game/Assets/Scripts/Item/StatType.cs
+++ chujuk-game/Assets/Scripts/Item/StatType.cs
@@ -11,9 +11,15 @@ public enum StatType
 
     GoldGain,        // 골드 획득량
 
-    DamageIncrease   // 전체 피해 증가
+    DamageIncrease,  // 전체 피해 증가
 
-    // 치명타 추가는 논의 사항 / 전체 데미지에 곱연산?
-    // CriticalChance,  // 치명타 확률
-    // CriticalDamage,  // 치명타 피해
+    ActiveCoolDown, // 액티브 쿨타임 감소
+
+    CriticalChance,  // 치명타 확률
+    CriticalDamage,  // 치명타 피해
+}
+public enum ModifierType
+{
+    Flat,       // 고정값 더하기 예) +10
+    Percent     // 퍼센트 예) +5%
 }
