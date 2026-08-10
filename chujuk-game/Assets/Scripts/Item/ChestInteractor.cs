5fef4cd493f02a45ca1f2d573118d9950c8a5f42
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2026-08-11 01:02:20 +0900

    아이템 추가

--- chujuk-game/Assets/Scripts/Item/ChestInteractor.cs
+++ chujuk-game/Assets/Scripts/Item/ChestInteractor.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class ChestInteractor : MonoBehaviour
 {
-    public GameObject itemToDrop; // 드롭할 아이템 프리팹
-    public Transform dropPoint;   // 아이템이 튀어나올 위치
+    [Header("아이템 데이터")]
+    public ItemData itemToDrop;
+    public Transform dropPoint; // 아이템이 튀어나올 위치
+
     private bool isPlayerInRange = false;
     private bool isOpened = false;
 
@@ -21,12 +23,22 @@ public class ChestInteractor : MonoBehaviour
         isOpened = true;
         Debug.Log("보물상자를 엽니다!");
 
-        // 아이템 생성
         if (itemToDrop != null)
         {
-            Instantiate(itemToDrop, dropPoint.position, Quaternion.identity);
-        }
+            // 빈 게임 오브젝트를 그 자리에서 즉석으로 생성
+            GameObject droppedItemObj = new GameObject("Dropped_" + itemToDrop.itemName);
+            droppedItemObj.transform.position = dropPoint.position;
 
+            // 아이콘을 보여줄 SpriteRenderer 컴포넌트 추가하고 이미지 세팅
+            SpriteRenderer sr = droppedItemObj.AddComponent<SpriteRenderer>();
+            sr.sprite = itemToDrop.icon;
+
+            // 아이템 먹는 콜라이더나 스크립트 추가
+            // BoxCollider2D col = droppedItemObj.AddComponent<BoxCollider2D>();
+            // col.isTrigger = true;
+
+            Debug.Log($"[{itemToDrop.itemName}] 아이템이 생성되었습니다!");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
